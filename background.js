f82a9a1aec5cf7ac34788b19151ae61d9efd07f3
dylanmcintyre <dylan@dmcintyre.net>
2015-12-09 11:15:38 -0500

    Support workspaces

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == "loading") {
-        if (tab.url.search("pivotal") != -1 && tab.url.search("/n/projects") != -1) {
+        if (tab.url.search("pivotal") != -1 && (tab.url.search("/n/projects") != -1 || tab.url.search("/n/workspaces") != -1)) {
             chrome.tabs.executeScript(tabId, {
                 file: 'run.js'
             });
